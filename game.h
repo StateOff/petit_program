e86ed09b7f9f5fd612b24e4afc0c8db492a8aad6
Blazej Floch <register@bfloch.com>
2021-03-28 00:15:13 -0400

    Refactoring game update and drawing

--- game.h
+++ game.h
@@ -30,8 +30,6 @@ public:
         GAMESTATE_INTRO,
         GAMESTATE_PLAYING,
         GAMESTATE_BITTEN,
-        GAMESTATE_ENEMY_WON,
-        GAMESTATE_PLAYER_WON,
         GAMESTATE_GAME_OVER,
     };
 
@@ -64,22 +62,255 @@ public:
         m_spriteAnimations[SPRITE_ANIMATION_TEXT_READY] = std::make_unique<SpriteAnimation>(m_spriteResources[SPRITE_TEXT], std::initializer_list<size_t>{8, 2, 5, 9, 10, 11});
         m_spriteAnimations[SPRITE_ANIMATION_TEXT_GAME_OVER] = std::make_unique<SpriteAnimation>(m_spriteResources[SPRITE_TEXT], std::initializer_list<size_t>{4, 5, 6, 2, 0, 7, 3, 2, 8});
 
-        // Center the gridd
-
         resetLevel();
     }
+
+
+    void update(Uint32 deltaTime)
+    {
+        switch (m_gameState)
+        {
+            case GAMESTATE_INTRO:
+                stateIntro(deltaTime);
+                break;
+            case GAMESTATE_BITTEN:
+                stateBitten(deltaTime);
+                break;
+            case GAMESTATE_PLAYING:
+                statePlaying(deltaTime);
+                break;
+        }
+    }
+
+    void draw()
+    {
+        for (auto& entity : m_entities)
+        {
+            if (entity == nullptr) continue;
+            entity->render(m_renderer);
+
+// Don't bother including this in release builds.
+#ifndef NDEBUG
+            // Debug information. Show collision Rect
+            if (DEBUG_COLLISION_RECT)
+            {
+                SpriteEntity* spriteEntity = dynamic_cast<SpriteEntity*>(entity.get());
+                if (spriteEntity == nullptr) continue;
+
+                SDL_Rect rect = spriteEntity->collisionRect();
+                SDL_SetRenderDrawColor(m_renderer, 0, 255, 0, 255);
+                SDL_RenderDrawRect(m_renderer, &rect);
+                SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 0);
+            }
+#endif
+        }
+    }
+
+private:
     void resetLevel()
     {
         m_entities.clear();
+
         m_numPoints = 0;
         m_gameState = GAMESTATE_INTRO;
         m_time = 0;
         m_numPointsPlayer = 0;
         m_numPointsEnemy = 0;
 
+        createPointsGrid();
+
+        createPlayers();
+
+        createHUD();
+    }
+
+    void statePlaying(Uint32 deltaTime)
+    {
+        // Update the player first and then do the collision check
+        Player* player = dynamic_cast<Player*>(m_entities[m_playerIndex].get());
+        SDL_Rect playerRect = player->collisionRect();
+
+        Enemy* enemy = dynamic_cast<Enemy*>(m_entities[m_enemyIndex].get());
+        SDL_Rect enemyRect = enemy->collisionRect();
+
+        player->update(deltaTime);
+
+        Counter* counterPlayer = dynamic_cast<Counter*>(m_entities[m_counterPlayerIndex].get());
+        Counter* counterEnemy = dynamic_cast<Counter*>(m_entities[m_counterEnemyIndex].get());
+
+        // -- Update and collision check
+
+        for (int i = 0; i < m_entities.size(); ++i)
+        {
+            auto& entity = m_entities[i];
+
+            if (i == m_playerIndex || entity == nullptr) continue;
+
+            Point* point = dynamic_cast<Point*>(entity.get());
+            if (point != nullptr)
+            {
+                if (m_numPointsPlayer + m_numPointsEnemy >= m_numPoints - 10) point->setSpeed(FAST_BLINK_SPEED);
+
+
+                SDL_Rect pointRect = point->collisionRect();
+                if (SDL_HasIntersection(&playerRect, &pointRect))
+                {
+                    m_entities[i] = nullptr;
+                    ++m_numPointsPlayer;
+                }
+                else if (SDL_HasIntersection(&enemyRect, &pointRect))
+                {
+                    m_entities[i] = nullptr;
+                    ++m_numPointsEnemy;
+                }
+                else
+                {
+                    entity->update(deltaTime);
+                }
+                enemy->setIsWinning(m_numPointsEnemy >= m_numPointsPlayer);
+            }
+            else
+            {
+                entity->update(deltaTime);
+            }
+        }
+
+        if (SDL_HasIntersection(&enemyRect, &playerRect))
+        {
+            m_gameState = GAMESTATE_BITTEN;
+
+            if (m_numPointsEnemy + m_numPointsPlayer == m_numPoints)
+            {
+                if (m_numPointsPlayer > m_numPointsEnemy)
+                {
+                    enemy->setAnimation(
+                        m_spriteAnimations[SPRITE_ANIMATION_PLAYER_2_BITTEN].get());
+                    enemy->setAnimationFrame(0);
+                    enemy->setAnimationSpeed(6);
+                    player->freeze();
+                    enemy->freeze();
+                    m_time = 0;
+                    m_level++;
+                    return;
+                }
+            }
+
+            player->setAnimation(
+                m_spriteAnimations[SPRITE_ANIMATION_PLAYER_1_BITTEN].get());
+            player->setAnimationFrame(0);
+            player->setAnimationSpeed(6);
+            player->freeze();
+            enemy->freeze();
+            enemy->setAnimationSpeed(0);
+            m_time = 0;
+        }
+
+        if (m_numPointsPlayer + m_numPointsEnemy == m_numPoints && m_numPointsPlayer > m_numPointsEnemy)
+        {
+            enemy->setAnimation(
+                m_spriteAnimations[SPRITE_ANIMATION_PLAYER_2_FEAR].get());
+        }
+
+        counterPlayer->setNumber(m_numPointsPlayer);
+        counterEnemy->setNumber(m_numPointsEnemy);
+    }
+
+    void stateBitten(Uint32 deltaTime)
+    {
+        Player* player = dynamic_cast<Player*>(m_entities[m_playerIndex].get());
+        player->update(deltaTime);
+
+        Enemy* enemy = dynamic_cast<Enemy*>(m_entities[m_enemyIndex].get());
+        enemy->update(deltaTime);
+
+        m_time += deltaTime;
+        if (m_time >= 1700)
+        {
+            if (m_level > LEVELS_COUNT)
+            {
+                m_gameState = GAMESTATE_GAME_OVER;
+
+                auto txtGameOver = std::make_unique<Text>(m_spriteAnimations, true);
+                txtGameOver->setPosition(BACKBUFFER_WIDTH / 2 + TEXT_WIDTH * 5, BACKBUFFER_HEIGHT / 2 - TEXT_HEIGHT);
+                txtGameOver->setText(m_spriteAnimations[SPRITE_ANIMATION_TEXT_GAME_OVER].get());
+                m_entities.emplace_back(std::move(txtGameOver));
+                m_textGameOverIndex = m_entities.size() - 1;
+
+                m_entities[m_playerIndex] = nullptr;
+            }
+            else
+            {
+                resetLevel();
+            }
+        }
+    }
+
+    void stateIntro(Uint32 deltaTime)
+    {
+        m_time += deltaTime;
+        if (m_time >= 3 * 1000)
+        {
+            m_gameState = GAMESTATE_PLAYING;
+            m_entities[m_textReadyIndex] = nullptr;
+        }
+    }
+
+    void createHUD()
+    {
+        // -- HUD: EnemyPoints - Level - Player Points
+
+        auto counterEnemy = std::make_unique<Counter>(m_spriteAnimations, false);
+        counterEnemy->setPosition(10, 2);
+        m_entities.emplace_back(std::move(counterEnemy));
+        m_counterEnemyIndex = m_entities.size() - 1;
+
+        auto counterPlayer = std::make_unique<Counter>(m_spriteAnimations, true);
+        counterPlayer->setPosition(BACKBUFFER_WIDTH - NUMBERS_WIDTH - 10, 2);
+        m_entities.emplace_back(std::move(counterPlayer));
+        m_counterPlayerIndex = m_entities.size() - 1;
+
+        auto txtLevel = std::make_unique<Text>(m_spriteAnimations, true);
+        txtLevel->setPosition(BACKBUFFER_WIDTH / 2, 2);
+        m_entities.emplace_back(std::move(txtLevel));
+
+        auto counterLevel = std::make_unique<Counter>(m_spriteAnimations, false);
+        counterLevel->setPosition(BACKBUFFER_WIDTH / 2 + TEXT_WIDTH * 2, 2);
+        counterLevel->setNumber(m_level);
+        m_entities.emplace_back(std::move(counterLevel));
+
+        // -- Ready text (is being destroyed at the beginning of the level)
+
+        auto txtReady = std::make_unique<Text>(m_spriteAnimations, true);
+        txtReady->setPosition(BACKBUFFER_WIDTH / 2 + TEXT_WIDTH * 3, BACKBUFFER_HEIGHT / 2 - TEXT_HEIGHT);
+        txtReady->setText(m_spriteAnimations[SPRITE_ANIMATION_TEXT_READY].get());
+        m_entities.emplace_back(std::move(txtReady));
+        m_textReadyIndex = m_entities.size() - 1;
+    }
+
+    void createPlayers()
+    {
+        // -- Player & Enemy
+
+        auto player = std::make_unique<Player>(m_spriteAnimations, true);
+        player->setPosition(BACKBUFFER_WIDTH - PLAYER_WIDTH - 20, BACKBUFFER_HEIGHT / 2 - PLAYER_HEIGHT / 2);
+        m_entities.emplace_back(std::move(player));
+        m_playerIndex = m_entities.size() - 1;
+
+        auto enemy = std::make_unique<Enemy>(m_spriteAnimations, false, m_entities, m_playerIndex);
+        enemy->setPosition(20, BACKBUFFER_HEIGHT / 2 - PLAYER_HEIGHT / 2);
+        m_entities.emplace_back(std::move(enemy));
+        m_enemyIndex = m_entities.size() - 1;
+    }
+
+    void createPointsGrid()
+    {
+        // -- Center the points grid
+
         constexpr int gridStep = 32;
         constexpr int gridOffsetX = (BACKBUFFER_WIDTH - ((BACKBUFFER_WIDTH / gridStep) - 1) * gridStep) / 2 - POINT_WIDTH / 2;
         constexpr int gridOffsetY = (BACKBUFFER_HEIGHT - ((BACKBUFFER_HEIGHT / gridStep) - 1) * gridStep) / 2 - POINT_HEIGHT / 2;
+
+        // -- Draw the points grid
 
         const unsigned char* level = &LEVELS[32 * (m_level - 1)];
         int i = 0;
@@ -94,197 +325,6 @@ public:
                     m_entities.emplace_back(std::move(point));
                     ++m_numPoints;
                 }
-            }
-        }
-
-        auto p = std::make_unique<Player>(m_spriteAnimations, true);
-        p->setPosition(BACKBUFFER_WIDTH - PLAYER_WIDTH - 20, BACKBUFFER_HEIGHT / 2 - PLAYER_HEIGHT / 2);
-        m_entities.emplace_back(std::move(p));
-        m_playerIndex = m_entities.size() - 1;
-
-        auto e = std::make_unique<Enemy>(m_spriteAnimations, false, m_entities, m_playerIndex);
-        e->setPosition(20, BACKBUFFER_HEIGHT / 2 - PLAYER_HEIGHT / 2);
-        m_entities.emplace_back(std::move(e));
-        m_enemyIndex = m_entities.size() - 1;
-
-        auto ce = std::make_unique<Counter>(m_spriteAnimations, false);
-        ce->setPosition(10, 2);
-        m_entities.emplace_back(std::move(ce));
-        m_counterEnemyIndex = m_entities.size() - 1;
-
-        auto cp = std::make_unique<Counter>(m_spriteAnimations, true);
-        cp->setPosition(BACKBUFFER_WIDTH - NUMBERS_WIDTH - 10, 2);
-        m_entities.emplace_back(std::move(cp));
-        m_counterPlayerIndex = m_entities.size() - 1;
-
-        auto txtLevel = std::make_unique<Text>(m_spriteAnimations, true);
-        txtLevel->setPosition(BACKBUFFER_WIDTH / 2, 2);
-        m_entities.emplace_back(std::move(txtLevel));
-
-        auto cLevel = std::make_unique<Counter>(m_spriteAnimations, false);
-        cLevel->setPosition(BACKBUFFER_WIDTH / 2 + TEXT_WIDTH * 2, 2);
-        cLevel->setNumber(m_level);
-        m_entities.emplace_back(std::move(cLevel));
-
-        auto txtReady = std::make_unique<Text>(m_spriteAnimations, true);
-        txtReady->setPosition(BACKBUFFER_WIDTH / 2 + TEXT_WIDTH * 3, BACKBUFFER_HEIGHT / 2 - TEXT_HEIGHT);
-        txtReady->setText(m_spriteAnimations[SPRITE_ANIMATION_TEXT_READY].get());
-        m_entities.emplace_back(std::move(txtReady));
-        m_textReadyIndex = m_entities.size() - 1;
-
-    }
-
-    void update(Uint32 deltaTime)
-    {
-        if (m_gameState == GAMESTATE_INTRO)
-        {
-            m_time += deltaTime;
-            if (m_time >= 3 * 1000)
-            {
-                m_gameState = GAMESTATE_PLAYING;
-                m_entities[m_textReadyIndex] = nullptr;
-            }
-            return;
-        }
-        else if (m_gameState == GAMESTATE_BITTEN)
-        {
-            Player* player = dynamic_cast<Player*>(m_entities[m_playerIndex].get());
-            player->update(deltaTime);
-
-            Enemy* enemy = dynamic_cast<Enemy*>(m_entities[m_enemyIndex].get());
-            enemy->update(deltaTime);
-
-            m_time += deltaTime;
-            if (m_time >= 1700)
-            {
-                if (m_level > LEVELS_COUNT)
-                {
-                    m_gameState = GAMESTATE_GAME_OVER;
-
-                    auto txtGameOver = std::make_unique<Text>(m_spriteAnimations, true);
-                    txtGameOver->setPosition(BACKBUFFER_WIDTH / 2 + TEXT_WIDTH * 5, BACKBUFFER_HEIGHT / 2 - TEXT_HEIGHT);
-                    txtGameOver->setText(m_spriteAnimations[SPRITE_ANIMATION_TEXT_GAME_OVER].get());
-                    m_entities.emplace_back(std::move(txtGameOver));
-                    m_textGameOverIndex = m_entities.size() - 1;
-
-                    m_entities[m_playerIndex] = nullptr;
-                }
-                else
-                {
-                    resetLevel();
-                }
-            }
-        }
-        else if (m_gameState == GAMESTATE_PLAYING)
-        {
-
-            // Update the player first and then do the collision check
-            Player* player = dynamic_cast<Player*>(m_entities[m_playerIndex].get());
-            SDL_Rect playerRect = player->collisionRect();
-
-            Enemy* enemy = dynamic_cast<Enemy*>(m_entities[m_enemyIndex].get());
-            SDL_Rect enemyRect = enemy->collisionRect();
-
-            player->update(deltaTime);
-
-            Counter* counterPlayer = dynamic_cast<Counter*>(m_entities[m_counterPlayerIndex].get());
-            Counter* counterEnemy = dynamic_cast<Counter*>(m_entities[m_counterEnemyIndex].get());
-
-            // Update and collison check
-            for (int i = 0; i < m_entities.size(); ++i)
-            {
-                auto& entity = m_entities[i];
-
-                if (i == m_playerIndex || entity == nullptr) continue;
-
-                Point* point = dynamic_cast<Point*>(entity.get());
-                if (point != nullptr)
-                {
-                    if (m_numPointsPlayer + m_numPointsEnemy >= m_numPoints - 10) point->setSpeed(FAST_BLINK_SPEED);
-
-
-                    SDL_Rect pointRect = point->collisionRect();
-                    if (SDL_HasIntersection(&playerRect, &pointRect))
-                    {
-                        m_entities[i] = nullptr;
-                        ++m_numPointsPlayer;
-                    }
-                    else if (SDL_HasIntersection(&enemyRect, &pointRect))
-                    {
-                        m_entities[i] = nullptr;
-                        ++m_numPointsEnemy;
-                    }
-                    else
-                    {
-                        entity->update(deltaTime);
-                    }
-                    enemy->setIsWinning(m_numPointsEnemy >= m_numPointsPlayer);
-
-                }
-                else
-                {
-                    entity->update(deltaTime);
-                }
-            }
-
-            if (SDL_HasIntersection(&enemyRect, &playerRect))
-            {
-                m_gameState = GAMESTATE_BITTEN;
-
-                if (m_numPointsEnemy + m_numPointsPlayer == m_numPoints)
-                {
-                    if (m_numPointsPlayer > m_numPointsEnemy)
-                    {
-                        enemy->setAnimation(
-                            m_spriteAnimations[SPRITE_ANIMATION_PLAYER_2_BITTEN].get());
-                        enemy->setAnimationFrame(0);
-                        enemy->setAnimationSpeed(6);
-                        player->freeze();
-                        enemy->freeze();
-                        m_time = 0;
-                        m_level++;
-                        return;
-                    }
-                }
-
-                player->setAnimation(
-                    m_spriteAnimations[SPRITE_ANIMATION_PLAYER_1_BITTEN].get());
-                player->setAnimationFrame(0);
-                player->setAnimationSpeed(6);
-                player->freeze();
-                enemy->freeze();
-                enemy->setAnimationSpeed(0);
-                m_time = 0;
-            }
-
-            if (m_numPointsPlayer + m_numPointsEnemy == m_numPoints && m_numPointsPlayer > m_numPointsEnemy)
-            {
-                enemy->setAnimation(
-                    m_spriteAnimations[SPRITE_ANIMATION_PLAYER_2_FEAR].get());
-            }
-
-            counterPlayer->setNumber(m_numPointsPlayer);
-            counterEnemy->setNumber(m_numPointsEnemy);
-        }
-    }
-
-    void draw()
-    {
-        for (auto& entity : m_entities)
-        {
-            if (entity == nullptr) continue;
-            entity->render(m_renderer);
-
-            // Debug information. Show collision Rect
-            if (DEBUG_COLLISION_RECT)
-            {
-                SpriteEntity* spriteEntity = dynamic_cast<SpriteEntity*>(entity.get());
-                if (spriteEntity == nullptr) continue;
-
-                SDL_Rect rect = spriteEntity->collisionRect();
-                SDL_SetRenderDrawColor(m_renderer, 0, 255, 0, 255);
-                SDL_RenderDrawRect(m_renderer, &rect);
-                SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 0);
             }
         }
     }
