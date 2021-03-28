70b0a275908c06c441e8ed5456953709638f7343
Blazej Floch <register@bfloch.com>
2021-03-28 02:42:43 -0400

    Color changes.

--- resources.h
+++ resources.h
@@ -100,8 +100,7 @@ constexpr unsigned char PLAYER_DATA[] =
     "         "
     "         "
     "         "
-    "         "
-;
+    "         ";
 
 constexpr int POINT_WIDTH = 4;
 constexpr int POINT_HEIGHT = 4;
@@ -116,8 +115,7 @@ constexpr unsigned char POINT_DATA[] =
     " !! "
     "!\"\"!"
     "!\"\"!"
-    " !! "
-;
+    " !! ";
 
 constexpr int NUMBERS_WIDTH = 5;
 constexpr int NUMBERS_HEIGHT = 5;
@@ -181,8 +179,7 @@ constexpr unsigned char NUMBERS_DATA[] =
     "!   !"
     " !!!!"
     "    !"
-    " !!! "
-;
+    " !!! ";
 
 constexpr int TEXT_WIDTH = 5;
 constexpr int TEXT_HEIGHT = 5;
@@ -270,24 +267,23 @@ constexpr unsigned char TEXT_DATA[] =
     "!  ! "
     "  !  "
     "     "
-    "  !  "
-;
+    "  !  ";
 
 
 static const std::array<SDL_Color, 5> palette1{{
     {.r = 0, .g = 0, .b = 0, .a = 0},        // Space
-    {.r = 255, .g = 255, .b = 255, .a = 255},    // !
-    {.r = 200, .g = 128, .b = 64, .a = 255},    // "
+    {.r = 255, .g = 255, .b = 255, .a = 255},// !
+    {.r = 200, .g = 128, .b = 64, .a = 255}, // "
     {.r = 226, .g = 240, .b = 220, .a = 255},// #
-    {.r = 124, .g = 180, .b = 255, .a = 255},      // $
+    {.r = 0, .g = 128, .b = 0, .a = 255},    // $
 }};
 
 static const std::array<SDL_Color, 5> palette2{{
     {.r = 0, .g = 0, .b = 0, .a = 0},        // Space
     {.r = 255, .g = 0, .b = 0, .a = 255},    // !
-    {.r = 200, .g = 128, .b = 64, .a = 255},    // "
-    {.r = 255, .g = 140, .b = 180, .a = 255},// #
-    {.r = 0, .g = 128, .b = 0, .a = 255},  // $
+    {.r = 200, .g = 128, .b = 64, .a = 255}, // "
+    {.r = 226, .g = 240, .b = 220, .a = 255},// #
+    {.r = 124, .g = 180, .b = 255, .a = 255},// $
 }};
 
 #endif//PETIT_PROGRAM_RESOURCES_H
